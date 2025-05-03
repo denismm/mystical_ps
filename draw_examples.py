dceb20d19070c325af1d2c6fe7b7b41a7c3412c0
Denis Moskowitz <denismm@gmail.com>
2025-05-03 19:20:55 -0400

    example xarray

---
+++ draw_examples.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import subprocess
+import sys
+
+examples: dict[str, list[int]] = {
+    'xarray_example': [-2, -2, 2.5, 2],
+}
+base_command = "pstopng -0.6 d d d 100 example.ps -Pimages/".split()
+for name, dimensions in examples.items():
+    command = base_command + [name]
+    command[1:5] = [str(d) for d in dimensions]
+    print(' '.join(command))
+    subprocess.run(command, check=True)
