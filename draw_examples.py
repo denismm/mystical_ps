6cdb5c905bb154b5c939135f5f1d39fd0b981e94
Denis Moskowitz <denismm@gmail.com>
2025-05-03 19:21:40 -0400

    example.ps to examples.ps

--- draw_examples.py
+++ draw_examples.py
@@ -5,7 +5,7 @@ import sys
 examples: dict[str, list[int]] = {
     'xarray_example': [-2, -2, 2.5, 2],
 }
-base_command = "pstopng -0.6 d d d 100 example.ps -Pimages/".split()
+base_command = "pstopng -0.6 d d d 100 examples.ps -Pimages/".split()
 for name, dimensions in examples.items():
     command = base_command + [name]
     command[1:5] = [str(d) for d in dimensions]
