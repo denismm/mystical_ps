91a0dbc19eead35de1d6d7cab3efa524d9d74f33
Denis Moskowitz <denismm@gmail.com>
2025-05-03 20:03:10 -0400

    cartouches for strings, array example

--- draw_examples.py
+++ draw_examples.py
@@ -4,6 +4,7 @@ import sys
 
 examples: dict[str, list[int]] = {
     'xarray_example': [-2, -2, 2.5, 2],
+    'array_example': [-2, -2, 2, 2],
 }
 base_command = "pstopng -0.6 d d d 100 examples.ps -Pimages/".split()
 for name, dimensions in examples.items():
