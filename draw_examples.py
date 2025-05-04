fa804b64723c6599642020bdac68f643adc0ab6d
Denis Moskowitz <denismm@gmail.com>
2025-05-03 22:21:40 -0400

    handle actual dicts as args to get_spell

--- draw_examples.py
+++ draw_examples.py
@@ -5,6 +5,7 @@ import sys
 examples: dict[str, list[int]] = {
     'xarray_example': [-2, -2, 2.5, 2],
     'array_example': [-2, -2, 2, 2],
+    'dict_example': [-2.5, -2.5, 2.5, 2.5],
 }
 base_command = "pstopng -0.6 d d d 100 examples.ps -Pimages/".split()
 for name, dimensions in examples.items():
