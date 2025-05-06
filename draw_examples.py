74d224ebb0ef1b8671d61eb82c93d8a3d85214cd
Denis Moskowitz <denismm@gmail.com>
2025-05-06 18:29:33 -0400

    token examples

--- draw_examples.py
+++ draw_examples.py
@@ -7,7 +7,12 @@ examples: dict[str, list[int]] = {
     'array_example': [-2, -2, 2, 2],
     'dict_example': [-2.5, -2.5, 2.5, 2.5],
     'link_example': [-1.75, -7, 5.5, 1.75],
-    'name_array': [-0.6, -0.6, 0.6, 0.6],
+    'operator_array': [-1.1, -1.1, 1.1, 1.1],
+    'name_array': [-1.1, -1.1, 1.1, 1.1],
+    'string_array': [-1.1, -1.1, 1.1, 1.1],
+    'operator_foo': [-1.1, -1.1, 1.1, 1.1],
+    'name_foo': [-1.1, -1.1, 1.1, 1.1],
+    'name_foobar': [-1.1, -1.1, 1.1, 1.1],
 }
 base_command = "pstopng -0.6 d d d 50 examples.ps -Pimages/".split()
 for name, dimensions in examples.items():
